efdbc0fab5d7a71d26eca14a396cab60bf54eb46
Angshuman Sarkar <angshus@thoughtworks.com>
2025-03-21 18:45:46 +0530

    BN-33 | Fix - build because of missing dependency version. Minor Cleanup. Added some comments for followup.

--- api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/providers/ConsultationBundleFhirR4ResourceProvider.java
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/providers/ConsultationBundleFhirR4ResourceProvider.java
@@ -20,11 +20,13 @@ import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Encounter;
 import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.ResourceType;
+import org.openmrs.module.fhir2.api.FhirConditionService;
 import org.openmrs.module.fhir2.api.FhirEncounterService;
 import org.openmrs.module.fhir2.api.annotations.R4Provider;
 import org.openmrs.module.fhir2.providers.util.FhirProviderUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,31 +49,40 @@ public class ConsultationBundleFhirR4ResourceProvider implements IResourceProvid
 	
 	@Create
     public MethodOutcome createBundle(@ResourceParam ConsultationBundle bundle) {
-        //TODO validate whether bundle type is transaction (later)
+		/**
+		 * We want to handle the whole request as a transaction.
+		 * - TODO: Define FHIR IG for the bundle.
+		 * - TODO: validate bundle type = transaction
+		 * - ensure all entries contain resources and request element
+		 * - resource references: can have server side references e.g. Patient/ABC12345
+		 */
 
-        //Rule check: to be defined in IG
-        // For all entries must contain resources, and have request elements
-        //resource references: can have server side references e.g. Patient/ABC12345
+		/**
+		 * TODO: The entire processing must be atomic. We can't use @trandsactional annotation
+		 * on the method to define transaction bounday in the provider, as spring creates a proxy around it,
+		 * and the HAPI FHIR servlet reflection method invocation will fail.
+		 * One way to solve is - create another service where the processing can be done.
+		 */
+
+        //For all entries must have resources and request elements
         List<Bundle.BundleEntryComponent> emptyResouceEntries = bundle.getEntry()
                 .stream().filter(entry -> !(entry.hasResource() && entry.hasRequest()))
                 .collect(Collectors.toList());
         if (!emptyResouceEntries.isEmpty()) {
-            throw new InvalidRequestException("Bundle entry must have resource && request defined");
+            throw new InvalidRequestException("Bundle entry must have resource & request defined");
         }
 
-        List<Resource> encounters = bundle.getEntry()
+        List<Bundle.BundleEntryComponent> encounterEntries = bundle.getEntry()
                 .stream().filter(entry -> entry.hasResource() && entry.getResource().getResourceType().equals(ResourceType.Encounter))
-                .map(entry -> entry.getResource())
                 .collect(Collectors.toList());
-        if (encounters.isEmpty()) {
+        if (encounterEntries.isEmpty()) {
             throw new InvalidRequestException("Bundle must accompany an encounter resource");
         }
 
-        //we expect additional visit resource to come in as well
-        //order the encounters and create them - visit first
-        //TODO not to be hardcoded. Must decide whether PUT or POST request and delegate to either create or update
-        MethodOutcome encounterCreationOutcome = FhirProviderUtils.buildCreate(encounterService.create((Encounter) encounters.get(0)));
-        //TODO code here
+        Bundle responseBundle = new ConsultationBundle();
+        //we expect additional visit resource (new visit to be created) to come in as well
+        //therefore we must process the FHIR Encounters in order - to OMRS visit first, then OMRS encounter
+        responseBundle.addEntry(processEncounter(encounterEntries.get(0)));
         //run through the rest of the entries in the bundle. process them in the order of dependencies. e.g obs can be part of another obs or other resources
         //delegate processing to resource specific OpenMRS Fhir Services such as observationFhirResourceProcessor, conditionFhirResourceProcessor etc
 
@@ -79,21 +90,34 @@ public class ConsultationBundleFhirR4ResourceProvider implements IResourceProvid
         /**
          * Consideration:
          * 1. We can restrict adding any resource def or operations on the patient resource.
-         * 2. We inject respective openmrs fhirresource providers and delegate to them, however
+         * 2. We inject respective openmrs fhirResource providers and delegate to them, however
          * there may be a way, we can get hold of all the resource providers, through
-         * Spring applciation context .. e.g. ctx.getBeansOfType(IResourceProvider.class) and using either reflection
-         * or the wayRestfulServer.handleRequest(..) method does, by determining registered resoruce providers handler method
-         * through determineResourceMethod(). this approach will save us a lot of boiler plate code
+         * Spring application context .. e.g. ctx.getBeansOfType(IResourceProvider.class) and using either reflection
+         * or the wayRestfulServer.handleRequest(..) method does, by determining registered resource providers handler method
+         * through determineResourceMethod(). e.g binding.getMethod().invoke(binding.getProvider(), theMethodParams)
+         * this approach will save us a lot of boilerplate code
          */
 
-        //the following code is temporary
         MethodOutcome methodOutcome = new MethodOutcome();
         methodOutcome.setCreated(true);
-        Resource newBundle = new ConsultationBundle();
-        if (newBundle != null) {
-            methodOutcome.setId(newBundle.getIdElement());
-            methodOutcome.setResource(newBundle);
-        }
+        methodOutcome.setResource(responseBundle);
         return methodOutcome;
     }
+	
+	private Bundle.BundleEntryComponent processEncounter(Bundle.BundleEntryComponent bundleEntryComponent) {
+		//TODO not to be hardcoded. Must decide whether PUT or POST request and delegate to either create or update
+		MethodOutcome encounterCreationOutcome = FhirProviderUtils.buildCreate(encounterService
+		        .create((Encounter) bundleEntryComponent.getResource()));
+		Bundle.BundleEntryComponent encounterEntry = new Bundle.BundleEntryComponent();
+		encounterEntry.setResource((Resource) encounterCreationOutcome.getResource());
+		Bundle.BundleEntryResponseComponent response = new Bundle.BundleEntryResponseComponent();
+		if (bundleEntryComponent.getRequest().getMethod().equals(Bundle.HTTPVerb.POST)) {
+			response.setStatus("201");
+		} else {
+			response.setStatus("200");
+		}
+		encounterEntry.setResponse(response);
+		return encounterEntry;
+		
+	}
 }
