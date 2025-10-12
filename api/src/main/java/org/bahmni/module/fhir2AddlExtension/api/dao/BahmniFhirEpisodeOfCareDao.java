9c14ba70729ada3ecae08c85352bcb9fe5114f7a
Angshuman Sarkar <angshus@thoughtworks.com>
2025-10-12 19:22:36 +0530

    removed unused import. added privilege check

--- api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/dao/BahmniFhirEpisodeOfCareDao.java
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/dao/BahmniFhirEpisodeOfCareDao.java
@@ -1,6 +1,5 @@
 package org.bahmni.module.fhir2AddlExtension.api.dao;
 
-import org.openmrs.Drug;
 import org.openmrs.annotation.Authorized;
 import org.openmrs.module.episodes.Episode;
 import org.openmrs.module.fhir2.api.dao.FhirDao;
@@ -12,18 +11,20 @@ import java.util.List;
 
 public interface BahmniFhirEpisodeOfCareDao extends FhirDao<Episode> {
 	
-	//@Authorized({"Get Concepts"})
+	@Authorized({"Get Episodes"})
 	Episode get(@Nonnull String uuid);
 	
-	//@Authorized({"Get Concepts"})
+	@Authorized({"Get Episodes"})
 	List<Episode> get(@Nonnull Collection<String> uuids);
-	
-	//@Authorized({"Manage Concepts"})
+
+	@Authorized({"Add Episodes"})
 	Episode createOrUpdate(@Nonnull Episode newEntry);
-	
+
+	@Authorized({"Delete Episodes"})
 	Episode delete(@Nonnull String uuid);
 	
 	int getSearchResultsCount(@Nonnull SearchParameterMap theParams);
-	
+
+	@Authorized({"Get Episodes"})
 	List<Episode> getSearchResults(@Nonnull SearchParameterMap theParams);
 }
--- api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/translator/BahmniEpisodeOfCareTranslator.java
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/translator/BahmniEpisodeOfCareTranslator.java
@@ -1,8 +1,6 @@
 package org.bahmni.module.fhir2AddlExtension.api.translator;
 
-import org.hl7.fhir.r4.model.AllergyIntolerance;
 import org.hl7.fhir.r4.model.EpisodeOfCare;
-import org.openmrs.Allergy;
 import org.openmrs.module.episodes.Episode;
 import org.openmrs.module.fhir2.api.translators.OpenmrsFhirTranslator;
 import org.openmrs.module.fhir2.api.translators.OpenmrsFhirUpdatableTranslator;
