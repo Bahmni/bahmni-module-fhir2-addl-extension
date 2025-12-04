5d48ca22fa7e2cd6b1ccd396ad76c4ced54046a9
Angshuman Sarkar <angshus@thoughtworks.com>
2025-12-04 09:15:50 +0530

    Added Tests

--- api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/translator/impl/BahmniFhirImagingStudyTranslatorImpl.java
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/translator/impl/BahmniFhirImagingStudyTranslatorImpl.java
@@ -1,61 +1,83 @@
 package org.bahmni.module.fhir2AddlExtension.api.translator.impl;
 
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
 import lombok.extern.slf4j.Slf4j;
+import org.bahmni.module.fhir2AddlExtension.api.model.FhirDocumentReference;
 import org.bahmni.module.fhir2AddlExtension.api.model.FhirImagingStudy;
 import org.bahmni.module.fhir2AddlExtension.api.translator.BahmniFhirImagingStudyTranslator;
 import org.bahmni.module.fhir2AddlExtension.api.translator.BahmniServiceRequestReferenceTranslator;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.ImagingStudy;
+import org.openmrs.Location;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.fhir2.api.translators.LocationReferenceTranslator;
 import org.openmrs.module.fhir2.api.translators.PatientReferenceTranslator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Nonnull;
+import java.util.Date;
+import java.util.List;
 
 @Component
 @Slf4j
 public class BahmniFhirImagingStudyTranslatorImpl implements BahmniFhirImagingStudyTranslator {
-
-    public static final String URN_DICOM_UID = "urn:dicom:uid";
+    public static final String SYSTEM_DICOM_UID = "urn:dicom:uid";
+    public static final String ERR_INVALID_LOCATION_REFERENCE = "Invalid location reference for imaging study";
     private final BahmniServiceRequestReferenceTranslator basedOnReferenceTranslator;
     private final PatientReferenceTranslator patientReferenceTranslator;
+    private final LocationReferenceTranslator locationReferenceTranslator;
 
     @Autowired
-    public BahmniFhirImagingStudyTranslatorImpl(BahmniServiceRequestReferenceTranslator basedOnReferenceTranslator, PatientReferenceTranslator patientReferenceTranslator) {
+    public BahmniFhirImagingStudyTranslatorImpl(BahmniServiceRequestReferenceTranslator basedOnReferenceTranslator, PatientReferenceTranslator patientReferenceTranslator, LocationReferenceTranslator locationReferenceTranslator) {
         this.basedOnReferenceTranslator = basedOnReferenceTranslator;
         this.patientReferenceTranslator = patientReferenceTranslator;
+        this.locationReferenceTranslator = locationReferenceTranslator;
     }
 
     @Override
-    public ImagingStudy toFhirResource(@Nonnull FhirImagingStudy data) {
+    public ImagingStudy toFhirResource(@Nonnull FhirImagingStudy study) {
         ImagingStudy resource = new ImagingStudy();
-        resource.setId(data.getUuid());
-
-        String studyInstanceUuid = data.getStudyInstanceUuid();
-        Identifier dicomStudyIdentifier = new Identifier();
-        dicomStudyIdentifier.setSystem(URN_DICOM_UID);
-        dicomStudyIdentifier.setValue(studyInstanceUuid);
-        resource.addIdentifier(dicomStudyIdentifier);
-
-
-        if (data.getOrder() != null) {
-            resource.addBasedOn(basedOnReferenceTranslator.toFhirResource(data.getOrder()));
-        }
-
-        if (data.getSubject() != null) {
-            resource.setSubject(patientReferenceTranslator.toFhirResource(data.getSubject()));
-        }
+        resource.setId(study.getUuid());
+        resource.addIdentifier(getStudyIdentifier(study));
+        resource.setStatus(tFhirStatusType(study.getStatus()));
+        resource.addBasedOn(basedOnReferenceTranslator.toFhirResource(study.getOrder()));
+        resource.setSubject(patientReferenceTranslator.toFhirResource(study.getSubject()));
+        resource.setLocation(locationReferenceTranslator.toFhirResource(study.getLocation()));
+        resource.setDescription(study.getDescription());
+        resource.setStarted(study.getDateStarted());
         return resource;
+    }
+
+    private Identifier getStudyIdentifier(FhirImagingStudy study) {
+        String studyInstanceUuid = study.getStudyInstanceUuid();
+        Identifier dicomStudyIdentifier = new Identifier();
+        dicomStudyIdentifier.setSystem(SYSTEM_DICOM_UID);
+        dicomStudyIdentifier.setValue(studyInstanceUuid);
+        return dicomStudyIdentifier;
     }
 
     @Override
     public FhirImagingStudy toOpenmrsType(@Nonnull ImagingStudy resource) {
         FhirImagingStudy study = new FhirImagingStudy();
+        study.setCreator(Context.getUserContext().getAuthenticatedUser());
+        study.setDateCreated(new Date());
         return this.toOpenmrsType(study, resource);
     }
 
     @Override
     public FhirImagingStudy toOpenmrsType(@Nonnull FhirImagingStudy existingObject, @Nonnull ImagingStudy resource) {
+        if (resource.hasIdentifier()) {
+            String studyInstanceUuid = findStudyInstanceUuid(resource.getIdentifier());
+            if (!StringUtils.isEmpty(studyInstanceUuid)) {
+                existingObject.setStudyInstanceUuid(studyInstanceUuid);
+            }
+        }
+
+        if (resource.hasStarted()) {
+            existingObject.setDateStarted(resource.getStarted());
+        }
 
         if (resource.hasSubject()) {
             existingObject.setSubject(patientReferenceTranslator.toOpenmrsType(resource.getSubject()));
@@ -65,6 +87,51 @@ public class BahmniFhirImagingStudyTranslatorImpl implements BahmniFhirImagingSt
             existingObject.setOrder(basedOnReferenceTranslator.toOpenmrsType(resource.getBasedOnFirstRep()));
         }
 
+        if (resource.hasStatus()) {
+            existingObject.setStatus(toOmrsStatusType(resource.getStatus()));
+        }
+
+        if (resource.hasLocation()) {
+            Location location = locationReferenceTranslator.toOpenmrsType(resource.getLocation());
+            if (location == null) {
+                log.error(ERR_INVALID_LOCATION_REFERENCE);
+                throw new InvalidRequestException(ERR_INVALID_LOCATION_REFERENCE);
+            }
+        }
+
+        if (resource.hasDescription()) {
+            existingObject.setDescription(resource.getDescription());
+        }
+
+        if (isExistingRecord(existingObject)) {
+            existingObject.setChangedBy(Context.getUserContext().getAuthenticatedUser());
+            existingObject.setDateChanged(new Date());
+        }
+
         return existingObject;
+    }
+
+    private String findStudyInstanceUuid(List<Identifier> identifiers) {
+        if (identifiers == null || identifiers.isEmpty()) {
+            return null;
+        }
+        for (Identifier identifier: identifiers) {
+            if (SYSTEM_DICOM_UID.equals(identifier.getSystem())) {
+                return identifier.getValue();
+            }
+        }
+        return null;
+    }
+
+    private FhirImagingStudy.FhirImagingStudyStatus toOmrsStatusType(ImagingStudy.ImagingStudyStatus status) {
+        return FhirImagingStudy.FhirImagingStudyStatus.valueOf(status.name());
+    }
+
+    private ImagingStudy.ImagingStudyStatus tFhirStatusType(FhirImagingStudy.FhirImagingStudyStatus status) {
+        return ImagingStudy.ImagingStudyStatus.valueOf(status.name());
+    }
+
+    private boolean isExistingRecord(FhirImagingStudy record) {
+        return record.getImagingStudyId() == null || record.getImagingStudyId() == 0;
     }
 }
---
+++ api/src/test/java/org/bahmni/module/fhir2AddlExtension/api/service/impl/BahmniFhirImagingStudyServiceImplTest.java
@@ -0,0 +1,115 @@
+package org.bahmni.module.fhir2AddlExtension.api.service.impl;
+
+import org.bahmni.module.fhir2AddlExtension.api.dao.BahmniFhirImagingStudyDao;
+import org.bahmni.module.fhir2AddlExtension.api.model.FhirImagingStudy;
+import org.bahmni.module.fhir2AddlExtension.api.service.BahmniFhirImagingStudyService;
+import org.bahmni.module.fhir2AddlExtension.api.translator.BahmniFhirImagingStudyTranslator;
+import org.bahmni.module.fhir2AddlExtension.api.translator.BahmniServiceRequestReferenceTranslator;
+import org.bahmni.module.fhir2AddlExtension.api.translator.impl.BahmniFhirImagingStudyTranslatorImpl;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.ImagingStudy;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.openmrs.Location;
+import org.openmrs.User;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.UserContext;
+import org.openmrs.api.db.ContextDAO;
+import org.openmrs.module.fhir2.api.translators.LocationReferenceTranslator;
+import org.openmrs.module.fhir2.api.translators.PatientReferenceTranslator;
+
+import java.io.IOException;
+
+import static org.bahmni.module.fhir2AddlExtension.api.TestDataFactory.loadResourceFromFile;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BahmniFhirImagingStudyServiceImplTest {
+    @Mock
+    private ContextDAO contextDAO;
+
+    @Mock
+    private UserContext userContext;
+
+    @Mock
+    private User user;
+
+    @Mock
+    private BahmniFhirImagingStudyDao imagingStudyDao;
+    @Mock
+    private BahmniServiceRequestReferenceTranslator basedOnReferenceTranslator;
+    @Mock
+    private PatientReferenceTranslator patientReferenceTranslator;
+    @Mock
+    private LocationReferenceTranslator locationReferenceTranslator;
+
+    private BahmniFhirImagingStudyTranslator imagingStudyTranslator;
+    private BahmniFhirImagingStudyService fhirImagingStudyService;
+
+    @Before
+    public void setUp() {
+        when(userContext.getAuthenticatedUser()).thenReturn(user);
+        Context.setDAO(contextDAO);
+        Context.openSession();
+        Context.setUserContext(userContext);
+
+        imagingStudyTranslator = new BahmniFhirImagingStudyTranslatorImpl(basedOnReferenceTranslator, patientReferenceTranslator, locationReferenceTranslator);
+        fhirImagingStudyService = new BahmniFhirImagingStudyServiceImpl(imagingStudyDao, imagingStudyTranslator) {
+            @Override
+            protected void validateObject(FhirImagingStudy object) {
+                //Done because baseFhirService implementation uses Context.getAdministrativeService()
+                //we are not mocking Context.serviceContext
+            }
+        };
+    }
+
+    @Test
+    public void shouldCreateImagingStudy() throws IOException {
+        Location studyLocation = new Location();
+        studyLocation.setName("Radiology Center");
+        studyLocation.setUuid("example-radiology-center");
+
+        IBaseResource fhirResource = loadResourceFromFile("example-imaging-study-registered.json");
+        when(imagingStudyDao.createOrUpdate(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        when(locationReferenceTranslator.toOpenmrsType(
+                ArgumentMatchers.argThat(reference -> {
+                    return reference.getReference().equals("Location/example-radiology-center");
+                })))
+                .thenReturn(studyLocation);
+        ImagingStudy imagingStudy = fhirImagingStudyService.create((ImagingStudy)  fhirResource);
+        //we are not considering client id for resoruces
+        Assert.assertFalse("Client Id is not accepted", imagingStudy.getId().equals("example-imaging-study"));
+        Assert.assertEquals(ImagingStudy.ImagingStudyStatus.REGISTERED, imagingStudy.getStatus());
+    }
+
+    @Test
+    public void shouldUpdateImagingStudy() throws IOException {
+        Location studyLocation = new Location();
+        studyLocation.setName("Radiology Center");
+        studyLocation.setUuid("example-radiology-center");
+
+        FhirImagingStudy existingStudy = new FhirImagingStudy();
+        existingStudy.setStudyInstanceUuid("urn:oid:2.16.124.113543.6003.1154777499.30246.19789.3503430045");
+        existingStudy.setStatus(FhirImagingStudy.FhirImagingStudyStatus.REGISTERED);
+
+        IBaseResource fhirResource = loadResourceFromFile("example-imaging-study-performed.json");
+        when(imagingStudyDao.get("example-imaging-study")).thenReturn(existingStudy);
+        when(imagingStudyDao.createOrUpdate(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        when(locationReferenceTranslator.toOpenmrsType(
+                ArgumentMatchers.argThat(reference -> {
+                    return reference.getReference().equals("Location/example-radiology-center");
+                })))
+                .thenReturn(studyLocation);
+        ImagingStudy imagingStudy = fhirImagingStudyService.update("example-imaging-study", (ImagingStudy)  fhirResource);
+        Assert.assertEquals(ImagingStudy.ImagingStudyStatus.REGISTERED, imagingStudy.getStatus());
+    }
+
+}
