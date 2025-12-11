f5b550d4b901465609f65c277fc59d4802f2201f
MOHANKUMAR T <mohankumar.t@thoughtworks.com>
2025-12-11 22:47:11 +0530

    BAH-4340 | Add. Event advice to raise atomfeed events on FHIR Encounter save and update

---
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/spring/Fhir2AddlExtensionAopConfiguration.java
@@ -0,0 +1,38 @@
+package org.bahmni.module.fhir2AddlExtension.spring;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bahmni.module.fhir2AddlExtension.advice.FhirEncounterSaveAdvice;
+import org.openmrs.module.fhir2.api.FhirEncounterService;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class Fhir2AddlExtensionAopConfiguration {
+	
+	@Bean
+	public FhirEncounterSaveAdvice fhirEncounterSaveAdvice() {
+		return new FhirEncounterSaveAdvice();
+	}
+	
+	@Bean
+	public Advisor createFhirEncounterSaveAdvisor(@Autowired FhirEncounterSaveAdvice fhirEncounterSaveAdvice) {
+		final List<String> SUPPORTED_METHODS = Arrays.asList("create", "update");
+		return new StaticMethodMatcherPointcutAdvisor(
+		                                              fhirEncounterSaveAdvice) {
+			
+			@Override
+			public boolean matches(Method method, Class<?> targetClass) {
+				return FhirEncounterService.class.isAssignableFrom(targetClass)
+				        && SUPPORTED_METHODS.contains(method.getName());
+			}
+		};
+	}
+}
