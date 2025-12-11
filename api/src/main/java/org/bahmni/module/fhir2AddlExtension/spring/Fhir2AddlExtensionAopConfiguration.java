8690b301df72cdcb014e2e65c0d0ba9699143eea
MOHANKUMAR T <mohankumar.t@thoughtworks.com>
2025-12-11 23:06:15 +0530

    BAH-4340 | Fix. Remove spring auto proxy annotation to fix issue with old APIs and double events

--- api/src/main/java/org/bahmni/module/fhir2AddlExtension/spring/Fhir2AddlExtensionAopConfiguration.java
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/spring/Fhir2AddlExtensionAopConfiguration.java
@@ -11,10 +11,8 @@ import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy
 public class Fhir2AddlExtensionAopConfiguration {
 	
 	@Bean
