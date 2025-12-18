ded583d71036d5c0c5b81cab546a2f052afc3662
Angshuman Sarkar <angshus@thoughtworks.com>
2025-12-18 23:11:45 +0530

    Fixing obs.groupMember setting through Observation.hasMember

--- api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/translator/impl/BahmniObservationTranslatorImpl.java
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/translator/impl/BahmniObservationTranslatorImpl.java
@@ -1,54 +1,210 @@
 package org.bahmni.module.fhir2AddlExtension.api.translator.impl;
 
+import lombok.AccessLevel;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.proxy.HibernateProxy;
 import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.StringType;
 import org.hl7.fhir.r4.model.Type;
+import org.openmrs.Concept;
+import org.openmrs.ConceptNumeric;
+import org.openmrs.Encounter;
 import org.openmrs.Obs;
-import org.openmrs.module.fhir2.api.translators.impl.ObservationTranslatorImpl;
+import org.openmrs.Patient;
+import org.openmrs.Person;
+import org.openmrs.api.db.hibernate.HibernateUtil;
+import org.openmrs.module.fhir2.FhirConstants;
+import org.openmrs.module.fhir2.api.translators.ConceptTranslator;
+import org.openmrs.module.fhir2.api.translators.EncounterReferenceTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationBasedOnReferenceTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationCategoryTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationEffectiveDatetimeTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationInterpretationTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationReferenceRangeTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationReferenceTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationStatusTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationTranslator;
+import org.openmrs.module.fhir2.api.translators.ObservationValueTranslator;
+import org.openmrs.module.fhir2.api.translators.PatientReferenceTranslator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
 
+import static org.apache.commons.lang3.Validate.notNull;
 import static org.bahmni.module.fhir2AddlExtension.api.BahmniFhirConstants.FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH;
+import static org.openmrs.module.fhir2.api.translators.impl.FhirTranslatorUtils.getLastUpdated;
+import static org.openmrs.module.fhir2.api.translators.impl.FhirTranslatorUtils.getVersionId;
+import static org.openmrs.module.fhir2.api.translators.impl.ReferenceHandlingTranslator.createLocationReferenceByUuid;
 
+/**
+ * Note, besides mapping the formNamespaceAndPath extension, this overloaded class fixes
+ * the obs group translation to openmrs, where instead of using obs.addGroupMember(member)
+ * it uses obs.setGroupMember(obs set). Otherwise, Hibernate throws error when the persisted obs
+ * are dirtied when obs.addGroupMember() tries to set the member obs' group, causing Hibernate to throw
+ * for attached transient group. Once we resolve the issue with OpenMRS,
+ * then the overridden toOpenmrsType(Obs obs, Observation resource, Supplier<Obs> groupedObsFactory)
+ * should be removed.
+ */
 @Component
 @Primary
-public class BahmniObservationTranslatorImpl extends ObservationTranslatorImpl {
-    @Override
-    public Obs toOpenmrsType(@Nonnull Observation fhirObservation) {
-        Obs obs = super.toOpenmrsType(fhirObservation);
-        obs.setFormNamespaceAndPath(mapFormNamespacePathExtension(fhirObservation));
-        return obs;
-    }
+@Setter(AccessLevel.PROTECTED)
+public class BahmniObservationTranslatorImpl implements ObservationTranslator {
+	
+	@Autowired
+	private ObservationStatusTranslator observationStatusTranslator;
+	
+	@Autowired
+	private ObservationReferenceTranslator observationReferenceTranslator;
+	
+	@Autowired
+	private ObservationValueTranslator observationValueTranslator;
+	
+	@Autowired
+	private ConceptTranslator conceptTranslator;
+	
+	@Autowired
+	private ObservationCategoryTranslator categoryTranslator;
+	
+	@Autowired
+	private EncounterReferenceTranslator<Encounter> encounterReferenceTranslator;
+	
+	@Autowired
+	private PatientReferenceTranslator patientReferenceTranslator;
+	
+	@Autowired
+	private ObservationInterpretationTranslator interpretationTranslator;
+	
+	@Autowired
+	private ObservationReferenceRangeTranslator referenceRangeTranslator;
+	
+	@Autowired
+	private ObservationBasedOnReferenceTranslator basedOnReferenceTranslator;
+	
+	@Autowired
+	private ObservationEffectiveDatetimeTranslator datetimeTranslator;
+	
+	@Override
+	public Obs toOpenmrsType(Obs obs, Observation resource, Supplier<Obs> groupedObsFactory) {
+		notNull(obs, "The existing Obs object should not be null");
+		notNull(resource, "The Observation object should not be null");
+		
+		observationStatusTranslator.toOpenmrsType(obs, resource.getStatus());
+		
+		obs.setEncounter(encounterReferenceTranslator.toOpenmrsType(resource.getEncounter()));
+		obs.setPerson(patientReferenceTranslator.toOpenmrsType(resource.getSubject()));
+		
+		Concept concept = conceptTranslator.toOpenmrsType(resource.getCode());
+		obs.setConcept(concept);
+		observationValueTranslator.toOpenmrsType(obs, resource.getValue());
 
-    private String mapFormNamespacePathExtension(Observation fhirObservation) {
-        List<Extension> extensions = fhirObservation.getExtensionsByUrl(FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH);
-        if (!extensions.isEmpty()) {
-            Extension formNameSpacePathExtn = extensions.get(0);
-            Type formNameSpacePathExtnValue = formNameSpacePathExtn.getValue();
-            return formNameSpacePathExtnValue.primitiveValue();
-        }
-        return null;
-    }
-
-    @Override
+		Set<Obs> members = new HashSet<>();
+		for (Reference reference : resource.getHasMember()) {
+			Obs member = observationReferenceTranslator.toOpenmrsType(reference);
+			members.add(member);
+		}
+		obs.setGroupMembers(members);
+		
+		if (!resource.getInterpretation().isEmpty()) {
+			interpretationTranslator.toOpenmrsType(obs, resource.getInterpretation().get(0));
+		}
+		
+		datetimeTranslator.toOpenmrsType(obs, resource.getEffectiveDateTimeType());
+		
+		if (resource.hasBasedOn()) {
+			obs.setOrder(basedOnReferenceTranslator.toOpenmrsType(resource.getBasedOn().get(0)));
+		}
+		
+		obs.setFormNamespaceAndPath(mapFormNamespacePathExtension(resource));
+		return obs;
+	}
+	
+	@Override
+	public Obs toOpenmrsType(@Nonnull Observation resource) {
+		notNull(resource, "The Observation object should not be null");
+		Obs obs = toOpenmrsType(new Obs(), resource);
+		obs.setFormNamespaceAndPath(mapFormNamespacePathExtension(resource));
+		return obs;
+	}
+	
+	private String mapFormNamespacePathExtension(Observation fhirObservation) {
+		List<Extension> extensions = fhirObservation.getExtensionsByUrl(FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH);
+		if (!extensions.isEmpty()) {
+			Extension formNameSpacePathExtn = extensions.get(0);
+			Type formNameSpacePathExtnValue = formNameSpacePathExtn.getValue();
+			return formNameSpacePathExtnValue.primitiveValue();
+		}
+		return null;
+	}
+	
+	@Override
     public Observation toFhirResource(@Nonnull Obs obs) {
-        Observation fhirResource = super.toFhirResource(obs);
+        notNull(obs, "The Obs object should not be null");
+
+        Observation resource = new Observation();
+        resource.setId(obs.getUuid());
+        resource.setStatus(observationStatusTranslator.toFhirResource(obs));
+
+        resource.setEncounter(encounterReferenceTranslator.toFhirResource(obs.getEncounter()));
+
+        Person obsPerson = obs.getPerson();
+        if (obsPerson != null) {
+            if (obsPerson instanceof HibernateProxy) {
+                obsPerson = HibernateUtil.getRealObjectFromProxy(obsPerson);
+            }
+
+            if (obsPerson instanceof Patient) {
+                resource.setSubject(patientReferenceTranslator.toFhirResource((Patient) obsPerson));
+            }
+        }
+
+        resource.setCode(conceptTranslator.toFhirResource(obs.getConcept()));
+        resource.addCategory(categoryTranslator.toFhirResource(obs.getConcept()));
+
+        if (obs.isObsGrouping()) {
+            for (Obs groupObs : obs.getGroupMembers()) {
+                if (!groupObs.getVoided()) {
+                    resource.addHasMember(observationReferenceTranslator.toFhirResource(groupObs));
+                }
+            }
+        }
+
+        resource.setValue(observationValueTranslator.toFhirResource(obs));
+
+        resource.addInterpretation(interpretationTranslator.toFhirResource(obs));
+
+        if (obs.getValueNumeric() != null) {
+            Concept concept = obs.getConcept();
+            if (concept instanceof ConceptNumeric) {
+                resource.setReferenceRange(referenceRangeTranslator.toFhirResource(obs));
+            }
+        }
+
+        if (obs.getValueText() != null && StringUtils.equals(obs.getComment(), "org.openmrs.Location")) {
+            resource.addExtension(FhirConstants.OPENMRS_FHIR_EXT_OBS_LOCATION_VALUE,
+                    createLocationReferenceByUuid(obs.getValueText()));
+        }
+
+        resource.setIssued(obs.getDateCreated());
+        resource.setEffective(datetimeTranslator.toFhirResource(obs));
+        resource.addBasedOn(basedOnReferenceTranslator.toFhirResource(obs.getOrder()));
+
+        resource.getMeta().setLastUpdated(getLastUpdated(obs));
+        resource.getMeta().setVersionId(getVersionId(obs));
+
         if (obs.getFormNamespaceAndPath() != null) {
             Optional.ofNullable(obs.getFormNamespaceAndPath())
-               .ifPresent(value -> fhirResource.addExtension(FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH, new StringType(value)));
+               .ifPresent(value -> resource.addExtension(FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH, new StringType(value)));
         }
-        return fhirResource;
-    }
-
-    @Override
-    public Obs toOpenmrsType(@Nonnull Obs existingObs, @Nonnull Observation observation) {
-        Obs obs = super.toOpenmrsType(existingObs, observation);
-        obs.setFormNamespaceAndPath(mapFormNamespacePathExtension(observation));
-        return obs;
+        return resource;
     }
 }
--- api/src/test/java/org/bahmni/module/fhir2AddlExtension/api/translator/impl/BahmniObservationTranslatorImplTest.java
+++ api/src/test/java/org/bahmni/module/fhir2AddlExtension/api/translator/impl/BahmniObservationTranslatorImplTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.openmrs.Concept;
+import org.openmrs.ConceptDatatype;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.module.fhir2.api.translators.ConceptTranslator;
@@ -23,104 +24,107 @@ import org.openmrs.module.fhir2.api.translators.ObservationReferenceTranslator;
 import org.openmrs.module.fhir2.api.translators.ObservationStatusTranslator;
 import org.openmrs.module.fhir2.api.translators.ObservationValueTranslator;
 import org.openmrs.module.fhir2.api.translators.PatientReferenceTranslator;
-import org.openmrs.module.fhir2.api.translators.impl.ObservationTranslatorImpl;
-
-import java.lang.reflect.Field;
 
 import static org.bahmni.module.fhir2AddlExtension.api.BahmniFhirConstants.FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BahmniObservationTranslatorImplTest {
+	
+	public static final String EXAMPLE_OBS_FORM_NAMESPACE_PATH = "Bahmni^Vitals.1/14-0";
+	
+	@Mock
+	private ObservationStatusTranslator observationStatusTranslator;
+	
+	@Mock
+	private ObservationReferenceTranslator observationReferenceTranslator;
+	
+	@Mock
+	private ObservationValueTranslator observationValueTranslator;
+	
+	@Mock
+	private ConceptTranslator conceptTranslator;
+	
+	@Mock
+	private ObservationCategoryTranslator categoryTranslator;
+	
+	@Mock
+	private EncounterReferenceTranslator<Encounter> encounterReferenceTranslator;
+	
+	@Mock
+	private PatientReferenceTranslator patientReferenceTranslator;
+	
+	@Mock
+	private ObservationInterpretationTranslator interpretationTranslator;
+	
+	@Mock
+	private ObservationReferenceRangeTranslator referenceRangeTranslator;
+	
+	@Mock
+	private ObservationBasedOnReferenceTranslator basedOnReferenceTranslator;
+	
+	@Mock
+	private ObservationEffectiveDatetimeTranslator datetimeTranslator;
+	
+	private BahmniObservationTranslatorImpl observationTranslator;
+	
+	@Before
+	public void setUp() throws Exception {
+		observationTranslator = new BahmniObservationTranslatorImpl();
+		observationTranslator.setObservationStatusTranslator(observationStatusTranslator);
+		observationTranslator.setObservationReferenceTranslator(observationReferenceTranslator);
+		observationTranslator.setObservationValueTranslator(observationValueTranslator);
+		observationTranslator.setConceptTranslator(conceptTranslator);
+		observationTranslator.setCategoryTranslator(categoryTranslator);
+		observationTranslator.setEncounterReferenceTranslator(encounterReferenceTranslator);
+		observationTranslator.setPatientReferenceTranslator(patientReferenceTranslator);
+		observationTranslator.setInterpretationTranslator(interpretationTranslator);
+		observationTranslator.setReferenceRangeTranslator(referenceRangeTranslator);
+		observationTranslator.setBasedOnReferenceTranslator(basedOnReferenceTranslator);
+		observationTranslator.setDatetimeTranslator(datetimeTranslator);
+	}
+	
+	@Test
+	public void shouldMapToOpenmrsObsFormNamespacePath() {
+		String CONCEPT_UUID = "obs_concept_uuid";
+		Concept concept = new Concept();
+		ConceptDatatype conceptDatatype = new ConceptDatatype();
+		concept.setUuid(CONCEPT_UUID);
+		concept.setDatatype(conceptDatatype);
+		
+		Observation resource = new Observation();
+		CodeableConcept codeableConcept = new CodeableConcept();
+		codeableConcept.setId(CONCEPT_UUID);
+		resource.setCode(codeableConcept);
+		
+		resource.addExtension(FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH, new StringType(EXAMPLE_OBS_FORM_NAMESPACE_PATH));
+		
+		when(conceptTranslator.toOpenmrsType(codeableConcept)).thenReturn(concept);
+		Obs result = observationTranslator.toOpenmrsType(new Obs(), resource);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(EXAMPLE_OBS_FORM_NAMESPACE_PATH, result.getFormNamespaceAndPath());
+	}
+	
+	@Test
+	public void shouldReturnFormNameSpaceAsObservationExtension() {
+		Obs observation = new Obs();
+		observation.setFormNamespaceAndPath(EXAMPLE_OBS_FORM_NAMESPACE_PATH);
+		Observation result = observationTranslator.toFhirResource(observation);
+		Assert.assertEquals(EXAMPLE_OBS_FORM_NAMESPACE_PATH,
+		    result.getExtensionByUrl(FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH).getValue().primitiveValue());
+	}
 
-    public static final String EXAMPLE_OBS_FORM_NAMESPACE_PATH = "Bahmni^Vitals.1/14-0";
-    @Mock
-    private ObservationStatusTranslator observationStatusTranslator;
+	/**
+	 * TBD: Commented off till we resolve the obsGroup issue in the translator.
+	 */
+//	private void setPropertyOnSuperClass(ObservationTranslatorImpl translator, String attributeName, Object value)
+//			throws NoSuchFieldException, IllegalAccessException {
+//		//TBD: unfortunately, the way the fhir2 ObservationTranslatorImpl beans are declared (package private),
+//		//and since BahmniObservationTranslatorImpl is from different package, hence usage of reflection
+//		Class<?> clazz = translator.getClass().getSuperclass();
+//		Field field = clazz.getDeclaredField(attributeName);
+//		field.setAccessible(true);
+//		field.set(translator, value);
+//  	}
 
-    @Mock
-    private ObservationReferenceTranslator observationReferenceTranslator;
-
-    @Mock
-    private ObservationValueTranslator observationValueTranslator;
-
-    @Mock
-    private ConceptTranslator conceptTranslator;
-
-    @Mock
-    private ObservationCategoryTranslator categoryTranslator;
-
-    @Mock
-    private EncounterReferenceTranslator<Encounter> encounterReferenceTranslator;
-
-    @Mock
-    private PatientReferenceTranslator patientReferenceTranslator;
-
-    @Mock
-    private ObservationInterpretationTranslator interpretationTranslator;
-
-    @Mock
-    private ObservationReferenceRangeTranslator referenceRangeTranslator;
-
-    @Mock
-    private ObservationBasedOnReferenceTranslator basedOnReferenceTranslator;
-
-    @Mock
-    private ObservationEffectiveDatetimeTranslator datetimeTranslator;
-
-    private BahmniObservationTranslatorImpl observationTranslator;
-
-    @Before
-    public void setUp() throws Exception {
-        observationTranslator = new BahmniObservationTranslatorImpl();
-
-        setPropertyOnSuperClass(observationTranslator, "observationStatusTranslator", observationStatusTranslator);
-        setPropertyOnSuperClass(observationTranslator, "observationReferenceTranslator", observationReferenceTranslator);
-        setPropertyOnSuperClass(observationTranslator, "observationValueTranslator", observationValueTranslator);
-        setPropertyOnSuperClass(observationTranslator, "conceptTranslator", conceptTranslator);
-        setPropertyOnSuperClass(observationTranslator, "categoryTranslator", categoryTranslator);
-        setPropertyOnSuperClass(observationTranslator, "encounterReferenceTranslator", encounterReferenceTranslator);
-        setPropertyOnSuperClass(observationTranslator, "patientReferenceTranslator", patientReferenceTranslator);
-        setPropertyOnSuperClass(observationTranslator, "interpretationTranslator", interpretationTranslator);
-        setPropertyOnSuperClass(observationTranslator, "referenceRangeTranslator", referenceRangeTranslator);
-        setPropertyOnSuperClass(observationTranslator, "basedOnReferenceTranslator", basedOnReferenceTranslator);
-        setPropertyOnSuperClass(observationTranslator, "datetimeTranslator", datetimeTranslator);
-    }
-
-    @Test
-    public void shouldMapToOpenmrsObsFormNamespacePath() {
-        String CONCEPT_UUID = "obs_concept_uuid";
-        Concept concept = new Concept();
-        concept.setUuid(CONCEPT_UUID);
-
-        Observation resource = new Observation();
-        CodeableConcept codeableConcept = new CodeableConcept();
-        codeableConcept.setId(CONCEPT_UUID);
-        resource.setCode(codeableConcept);
-
-        resource.addExtension(FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH, new StringType(EXAMPLE_OBS_FORM_NAMESPACE_PATH));
-
-        when(conceptTranslator.toOpenmrsType(codeableConcept)).thenReturn(concept);
-        Obs result = observationTranslator.toOpenmrsType(new Obs(), resource);
-        Assert.assertNotNull(result);
-        Assert.assertEquals(EXAMPLE_OBS_FORM_NAMESPACE_PATH, result.getFormNamespaceAndPath());
-    }
-
-    @Test
-    public void shouldReturnFormNameSpaceAsObservationExtension() {
-        Obs observation = new Obs();
-        observation.setFormNamespaceAndPath(EXAMPLE_OBS_FORM_NAMESPACE_PATH);
-        Observation result = observationTranslator.toFhirResource(observation);
-        Assert.assertEquals(EXAMPLE_OBS_FORM_NAMESPACE_PATH,
-                result.getExtensionByUrl(FHIR_EXT_OBSERVATION_FORM_NAMESPACE_PATH).getValue().primitiveValue());
-    }
-
-    private void setPropertyOnSuperClass(ObservationTranslatorImpl translator, String attributeName, Object value)
-            throws NoSuchFieldException, IllegalAccessException {
-        //TBD: unfortunately, the way the fhir2 ObservationTranslatorImpl beans are declared (package private),
-        //and since BahmniObservationTranslatorImpl is from different package, hence usage of reflection
-        Class<?> clazz = translator.getClass().getSuperclass();
-        Field field = clazz.getDeclaredField(attributeName);
-        field.setAccessible(true);
-        field.set(translator, value);
-    }
 }
