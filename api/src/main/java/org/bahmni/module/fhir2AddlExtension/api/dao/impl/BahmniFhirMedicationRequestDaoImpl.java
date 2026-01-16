aa22c057bfacad6d2280bb480d9d189ae36b5656
MOHANKUMAR T <mohankumar.t@thoughtworks.com>
2026-01-16 13:07:44 +0530

    BAH-4314 | Add. Support for filtering MedicationsRequest by code.system URL

--- api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/dao/impl/BahmniFhirMedicationRequestDaoImpl.java
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/dao/impl/BahmniFhirMedicationRequestDaoImpl.java
@@ -1,5 +1,7 @@
 package org.bahmni.module.fhir2AddlExtension.api.dao.impl;
 
+import org.bahmni.module.fhir2AddlExtension.api.utils.ModuleUtils;
+import org.hibernate.criterion.Criterion;
 import org.openmrs.DrugOrder;
 import org.openmrs.api.OrderService;
 import org.openmrs.module.fhir2.api.dao.impl.FhirMedicationRequestDaoImpl;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 @Component
 @Primary
@@ -19,5 +22,13 @@ public class BahmniFhirMedicationRequestDaoImpl extends FhirMedicationRequestDao
 	@Override
 	public DrugOrder createOrUpdate(@Nonnull DrugOrder newEntry) {
 		return (DrugOrder) orderService.saveOrder(newEntry, null);
+	}
+	
+	@Override
+	protected Criterion generateSystemQuery(String system, List<String> codes, String conceptReferenceTermAlias) {
+		if (ModuleUtils.isConceptReferenceCodeEmpty(codes)) {
+			return ModuleUtils.generateSystemQueryForEmptyCodes(system, conceptReferenceTermAlias);
+		}
+		return super.generateSystemQuery(system, codes, conceptReferenceTermAlias);
 	}
 }
---
+++ api/src/test/java/org/bahmni/module/fhir2AddlExtension/api/dao/impl/BahmniFhirMedicationRequestDaoImplTest.java
@@ -0,0 +1,69 @@
+package org.bahmni.module.fhir2AddlExtension.api.dao.impl;
+
+import org.bahmni.module.fhir2AddlExtension.api.utils.ModuleUtils;
+import org.hibernate.criterion.Criterion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ModuleUtils.class)
+@PowerMockIgnore({ "javax.management.*", "javax.script.*", "org.apache.logging.log4j.*" })
+public class BahmniFhirMedicationRequestDaoImplTest {
+	
+	private static final String SYSTEM_URL = "http://example.com/system";
+	
+	private static final String CONCEPT_REFERENCE_TERM_ALIAS = "crt";
+	
+	@Mock
+	private Criterion mockCriterion;
+	
+	@InjectMocks
+	private BahmniFhirMedicationRequestDaoImpl bahmniFhirMedicationRequestDao;
+	
+	@Test
+	public void generateSystemQuery_shouldCallModuleUtilsWhenCodesIsEmpty() {
+		PowerMockito.mockStatic(ModuleUtils.class);
+		
+		when(ModuleUtils.isConceptReferenceCodeEmpty(null)).thenReturn(true);
+		when(ModuleUtils.generateSystemQueryForEmptyCodes(SYSTEM_URL, CONCEPT_REFERENCE_TERM_ALIAS)).thenReturn(
+		    mockCriterion);
+		
+		bahmniFhirMedicationRequestDao.generateSystemQuery(SYSTEM_URL, null, CONCEPT_REFERENCE_TERM_ALIAS);
+		
+		verifyStatic(ModuleUtils.class);
+		ModuleUtils.isConceptReferenceCodeEmpty(null);
+		
+		verifyStatic(ModuleUtils.class);
+		ModuleUtils.generateSystemQueryForEmptyCodes(SYSTEM_URL, CONCEPT_REFERENCE_TERM_ALIAS);
+	}
+	
+	@Test
+	public void generateSystemQuery_shouldNotCallGenerateSystemQueryForEmptyCodesWhenCodesHasValues() {
+		List<String> codes = Collections.singletonList("validCode");
+		
+		PowerMockito.mockStatic(ModuleUtils.class);
+		
+		when(ModuleUtils.isConceptReferenceCodeEmpty(codes)).thenReturn(false);
+		
+		bahmniFhirMedicationRequestDao.generateSystemQuery(SYSTEM_URL, codes, CONCEPT_REFERENCE_TERM_ALIAS);
+		
+		verifyStatic(ModuleUtils.class);
+		ModuleUtils.isConceptReferenceCodeEmpty(codes);
+		
+		verifyStatic(ModuleUtils.class, never());
+		ModuleUtils.generateSystemQueryForEmptyCodes(SYSTEM_URL, CONCEPT_REFERENCE_TERM_ALIAS);
+	}
+}
