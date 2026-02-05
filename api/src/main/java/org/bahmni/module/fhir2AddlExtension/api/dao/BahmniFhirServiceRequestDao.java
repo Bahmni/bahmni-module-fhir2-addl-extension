d0341b6d673452eb5a00d12caf96988ee0bc7c4d
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2026-02-05 15:19:28 +0530

    BAH-4420 | Fix. Creation of linked orders validation error. (#43)
    - This commit fixes the validation error thrown by OpenMRS Service for the case where previous order should have the same concept

--- api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/dao/BahmniFhirServiceRequestDao.java
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/dao/BahmniFhirServiceRequestDao.java
@@ -17,4 +17,7 @@ public interface BahmniFhirServiceRequestDao<T extends OpenmrsObject & Auditable
 	@Authorized({ "Edit Orders" })
 	T updateOrder(T order);
 	
+	@Authorized({ "Add Orders", "Edit Orders" })
+	void linkOrder(T order, T previousOrder);
+	
 }
