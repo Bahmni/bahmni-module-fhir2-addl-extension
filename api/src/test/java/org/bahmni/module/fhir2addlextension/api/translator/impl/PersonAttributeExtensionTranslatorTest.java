9fabd30fb126502e5d2ea53d3981f500dc0598df
Varun Prasad <varun.prasad@thoughtworks.com>
2026-05-19 11:10:00 +0530

    BAH-4679 | Fix. tests

--- api/src/test/java/org/bahmni/module/fhir2addlextension/api/translator/impl/PersonAttributeExtensionTranslatorTest.java
+++ api/src/test/java/org/bahmni/module/fhir2addlextension/api/translator/impl/PersonAttributeExtensionTranslatorTest.java
@@ -7,10 +7,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.openmrs.PersonAttribute;
 import org.openmrs.PersonAttributeType;
 import org.openmrs.api.PersonService;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.UserContext;
+import org.openmrs.api.db.ContextDAO;
+import org.openmrs.User;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 
@@ -19,82 +23,86 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PersonAttributeExtensionTranslatorTest {
-	
+
 	private static final String PREFIX = "http://fhir.bahmni.org/ext/patient/";
-	
+
 	@Mock
 	private PersonService personService;
-	
+
+	@Mock
+	private UserContext userContext;
+
+	@Mock
+	private ContextDAO contextDAO;
+
+	@Mock
+	private User user;
+
 	private PersonAttributeExtensionTranslator translator;
-	
+
 	private PersonAttributeType phoneType;
-	
+
 	private PersonAttributeType boolType;
-	
+
 	@Before
 	public void setup() {
-		translator = new PersonAttributeExtensionTranslator(personService);
-		
+		when(userContext.getAuthenticatedUser()).thenReturn(user);
+		Context.setDAO(contextDAO);
+		Context.openSession();
+		Context.setUserContext(userContext);
+
+		translator = new PersonAttributeExtensionTranslator();
+
 		phoneType = new PersonAttributeType();
 		phoneType.setUuid("phone-uuid");
 		phoneType.setName("phoneNumber");
 		phoneType.setFormat("java.lang.String");
-		
+
 		boolType = new PersonAttributeType();
 		boolType.setUuid("bool-uuid");
 		boolType.setName("isVIP");
 		boolType.setFormat("org.openmrs.customdatatype.datatype.BooleanDatatype");
-		
-		when(personService.getAllPersonAttributeTypes(false)).thenReturn(Arrays.asList(phoneType, boolType));
 	}
-	
+
 	@Test
 	public void toFhirResource_shouldConvertStringAttribute() {
 		PersonAttribute attr = new PersonAttribute(phoneType, "+919876543210");
-		
+
 		Extension ext = translator.toFhirResource(attr);
-		
+
 		assertEquals(PREFIX + "phonenumber", ext.getUrl());
 		assertEquals("+919876543210", ((StringType) ext.getValue()).getValue());
 	}
-	
+
 	@Test
 	public void toFhirResource_shouldConvertBooleanAttribute() {
 		PersonAttribute attr = new PersonAttribute(boolType, "true");
-		
+
 		Extension ext = translator.toFhirResource(attr);
-		
+
 		assertEquals(PREFIX + "isvip", ext.getUrl());
 		assertTrue(((BooleanType) ext.getValue()).booleanValue());
 	}
-	
+
 	@Test
 	public void toFhirResource_shouldReturnNullForNullValue() {
 		PersonAttribute attr = new PersonAttribute(phoneType, null);
-		
+
 		assertNull(translator.toFhirResource(attr));
 	}
-	
-	@Test
-	public void resolveType_shouldMatchBySlugName() {
-		PersonAttributeType result = translator.resolveType(PREFIX + "phonenumber");
-		
-		assertNotNull(result);
-		assertEquals("phone-uuid", result.getUuid());
-	}
-	
-	@Test
-	public void resolveType_shouldReturnNullForUnknownSlug() {
-		assertNull(translator.resolveType(PREFIX + "unknown"));
-	}
-	
+
 	@Test
 	public void resolveType_shouldReturnNullForNonPatientUrl() {
 		assertNull(translator.resolveType("http://fhir.bahmni.org/ext/service-request/something"));
 	}
-	
+
 	@Test
 	public void resolveType_shouldReturnNullForNullUrl() {
 		assertNull(translator.resolveType(null));
+	}
+
+	@Test
+	public void resolveType_shouldReturnNullForEmptySlug() {
+		assertNull(translator.resolveType(PREFIX));
 	}
 }
