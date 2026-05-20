00b35edea8c189c7d12957aa69e5142b44b50157
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2026-05-20 09:53:48 +0530

    BAH-4715 | Translate MedicationRequest Status based on fulfiller_status and dates (#70)
    * BAH-4715 | Refactor. Remove Medication Request Status Calculator and merge in Status translator
    
    * BAH-4715 | Refactor. Medication Request Status mapping based on fulfiller_status field also
    
    * BAH-4715 | Refactor. Break long method into smaller unit

--- api/src/test/java/org/bahmni/module/fhir2addlextension/api/translator/impl/BahmniMedicationRequestStatusTranslatorImplTest.java
+++ api/src/test/java/org/bahmni/module/fhir2addlextension/api/translator/impl/BahmniMedicationRequestStatusTranslatorImplTest.java
@@ -11,37 +11,22 @@ package org.bahmni.module.fhir2addlextension.api.translator.impl;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
-import static org.mockito.Mockito.when;
 
+import java.lang.reflect.Field;
 import java.util.Calendar;
 import java.util.Date;
 
 import org.hl7.fhir.r4.model.MedicationRequest;
-import org.bahmni.module.fhir2addlextension.api.translator.MedicationStatusCalculator;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.openmrs.DrugOrder;
 import org.openmrs.Order;
 
-/**
- * Unit tests for BahmniMedicationRequestStatusTranslatorImpl. Tests the integration between the
- * translator and the status calculator interface.
- */
-@RunWith(MockitoJUnitRunner.class)
 public class BahmniMedicationRequestStatusTranslatorImplTest {
 	
 	private static final String DRUG_ORDER_UUID = "44fdc8ad-fe4d-499b-93a8-8a991c1d477e";
 	
-	@Mock
-	private MedicationStatusCalculator statusCalculator;
-	
-	@InjectMocks
 	private BahmniMedicationRequestStatusTranslatorImpl statusTranslator;
 	
 	private DrugOrder drugOrder;
@@ -54,7 +39,8 @@ public class BahmniMedicationRequestStatusTranslatorImplTest {
 	
 	@Before
 	public void setup() {
-		// Setup test dates
+		statusTranslator = new BahmniMedicationRequestStatusTranslatorImpl();
+		
 		Calendar cal = Calendar.getInstance();
 		today = cal.getTime();
 		
@@ -64,7 +50,6 @@ public class BahmniMedicationRequestStatusTranslatorImplTest {
 		cal.add(Calendar.DAY_OF_MONTH, 2);
 		tomorrow = cal.getTime();
 		
-		// Setup basic drug order
 		drugOrder = new DrugOrder();
 		drugOrder.setUuid(DRUG_ORDER_UUID);
 		drugOrder.setDateActivated(yesterday);
@@ -72,138 +57,213 @@ public class BahmniMedicationRequestStatusTranslatorImplTest {
 		drugOrder.setVoided(false);
 	}
 	
-	// ========== INTEGRATION TESTS WITH STATUS CALCULATOR ==========
-	
 	@Test
 	public void toFhirResource_shouldReturnNull_whenDrugOrderIsNull() {
-		// When: Translate null drug order
-		MedicationRequest.MedicationRequestStatus status = statusTranslator.toFhirResource(null);
-		
-		// Then: Should return null
-		assertThat(status, nullValue());
+		assertThat(statusTranslator.toFhirResource(null), nullValue());
 	}
 	
 	@Test
-	public void toFhirResource_shouldDelegateToCalculator_andReturnActive() {
-		// Given: Status calculator returns ACTIVE
-		when(statusCalculator.calculateStatus(drugOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.ACTIVE);
-		
-		// When: Translate to FHIR status
-		MedicationRequest.MedicationRequestStatus status = statusTranslator.toFhirResource(drugOrder);
-		
-		// Then: Should return ACTIVE from calculator
-		assertThat(status, equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	public void toFhirResource_shouldReturnEnteredInError_whenOrderIsVoided() {
+		drugOrder.setVoided(true);
+		assertThat(statusTranslator.toFhirResource(drugOrder),
+		    equalTo(MedicationRequest.MedicationRequestStatus.ENTEREDINERROR));
 	}
 	
 	@Test
-	public void toFhirResource_shouldDelegateToCalculator_andReturnOnHold() {
-		// Given: Status calculator returns ON_HOLD
-		when(statusCalculator.calculateStatus(drugOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.ONHOLD);
-		
-		// When: Translate to FHIR status
-		MedicationRequest.MedicationRequestStatus status = statusTranslator.toFhirResource(drugOrder);
-		
-		// Then: Should return ON_HOLD from calculator
-		assertThat(status, equalTo(MedicationRequest.MedicationRequestStatus.ONHOLD));
+	public void toFhirResource_shouldReturnCompleted_whenFulfillerStatusIsCompleted() {
+		drugOrder.setFulfillerStatus(Order.FulfillerStatus.COMPLETED);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.COMPLETED));
 	}
 	
 	@Test
-	public void toFhirResource_shouldDelegateToCalculator_andReturnStopped() {
-		// Given: Status calculator returns STOPPED
-		when(statusCalculator.calculateStatus(drugOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.STOPPED);
-		
-		// When: Translate to FHIR status
-		MedicationRequest.MedicationRequestStatus status = statusTranslator.toFhirResource(drugOrder);
-		
-		// Then: Should return STOPPED from calculator
-		assertThat(status, equalTo(MedicationRequest.MedicationRequestStatus.STOPPED));
+	public void toFhirResource_shouldReturnActive_whenFulfillerStatusIsInProgress() {
+		drugOrder.setFulfillerStatus(Order.FulfillerStatus.IN_PROGRESS);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
 	}
 	
 	@Test
-	public void toFhirResource_shouldDelegateToCalculator_andReturnCancelled() {
-		// Given: Status calculator returns CANCELLED
-		when(statusCalculator.calculateStatus(drugOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.CANCELLED);
-		
-		// When: Translate to FHIR status
-		MedicationRequest.MedicationRequestStatus status = statusTranslator.toFhirResource(drugOrder);
-		
-		// Then: Should return CANCELLED from calculator
-		assertThat(status, equalTo(MedicationRequest.MedicationRequestStatus.CANCELLED));
+	public void toFhirResource_shouldReturnActive_whenFulfillerStatusIsReceived() {
+		drugOrder.setFulfillerStatus(Order.FulfillerStatus.RECEIVED);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
 	}
 	
 	@Test
-	public void toFhirResource_shouldDelegateToCalculator_andReturnCompleted() {
-		// Given: Status calculator returns COMPLETED
-		when(statusCalculator.calculateStatus(drugOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.COMPLETED);
-		
-		// When: Translate to FHIR status
-		MedicationRequest.MedicationRequestStatus status = statusTranslator.toFhirResource(drugOrder);
-		
-		// Then: Should return COMPLETED from calculator
-		assertThat(status, equalTo(MedicationRequest.MedicationRequestStatus.COMPLETED));
+	public void toFhirResource_shouldReturnStopped_whenFulfillerStatusIsExceptionAndDateStoppedIsSet() throws Exception {
+		drugOrder.setFulfillerStatus(Order.FulfillerStatus.EXCEPTION);
+		setDateStopped(drugOrder, yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.STOPPED));
 	}
 	
 	@Test
-	public void toFhirResource_shouldDelegateToCalculator_andReturnUnknown() {
-		// Given: Status calculator returns UNKNOWN
-		when(statusCalculator.calculateStatus(drugOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.UNKNOWN);
-		
-		// When: Translate to FHIR status
-		MedicationRequest.MedicationRequestStatus status = statusTranslator.toFhirResource(drugOrder);
-		
-		// Then: Should return UNKNOWN from calculator
-		assertThat(status, equalTo(MedicationRequest.MedicationRequestStatus.UNKNOWN));
+	public void toFhirResource_shouldReturnUnknown_whenFulfillerStatusIsExceptionAndNoDateStopped() {
+		drugOrder.setFulfillerStatus(Order.FulfillerStatus.EXCEPTION);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.UNKNOWN));
 	}
 	
 	@Test
-	public void toFhirResource_shouldHandleCalculatorReturningNull() {
-		// Given: Status calculator returns null
-		when(statusCalculator.calculateStatus(drugOrder)).thenReturn(null);
-		
-		// When: Translate to FHIR status
-		MedicationRequest.MedicationRequestStatus status = statusTranslator.toFhirResource(drugOrder);
-		
-		// Then: Should return null
-		assertThat(status, nullValue());
+	public void toFhirResource_shouldReturnCompleted_whenActionIsDiscontinue() {
+		drugOrder.setAction(Order.Action.DISCONTINUE);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.COMPLETED));
 	}
 	
 	@Test
-	public void toFhirResource_shouldBeConsistent_acrossMultipleCalls() {
-		// Given: Status calculator consistently returns ACTIVE
-		when(statusCalculator.calculateStatus(drugOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.ACTIVE);
-		
-		// When: Translate multiple times
-		MedicationRequest.MedicationRequestStatus status1 = statusTranslator.toFhirResource(drugOrder);
-		MedicationRequest.MedicationRequestStatus status2 = statusTranslator.toFhirResource(drugOrder);
-		MedicationRequest.MedicationRequestStatus status3 = statusTranslator.toFhirResource(drugOrder);
-		
-		// Then: All calls should return the same result
-		assertThat(status1, equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
-		assertThat(status2, equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
-		assertThat(status3, equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
-		assertThat(status1, equalTo(status2));
-		assertThat(status2, equalTo(status3));
+	public void toFhirResource_shouldReturnCompleted_whenDiscontinuedEvenIfAutoExpired() {
+		drugOrder.setAction(Order.Action.DISCONTINUE);
+		drugOrder.setAutoExpireDate(yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.COMPLETED));
 	}
 	
 	@Test
-	public void toFhirResource_shouldHandleDifferentDrugOrders() {
-		// Given: Different drug orders
-		DrugOrder activeOrder = new DrugOrder();
-		activeOrder.setUuid("active-order");
-		
-		DrugOrder stoppedOrder = new DrugOrder();
-		stoppedOrder.setUuid("stopped-order");
-		
-		// And: Calculator returns different statuses for different orders
-		when(statusCalculator.calculateStatus(activeOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.ACTIVE);
-		when(statusCalculator.calculateStatus(stoppedOrder)).thenReturn(MedicationRequest.MedicationRequestStatus.STOPPED);
-		
-		// When: Translate both orders
-		MedicationRequest.MedicationRequestStatus activeStatus = statusTranslator.toFhirResource(activeOrder);
-		MedicationRequest.MedicationRequestStatus stoppedStatus = statusTranslator.toFhirResource(stoppedOrder);
-		
-		// Then: Should return different statuses as calculated
-		assertThat(activeStatus, equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
-		assertThat(stoppedStatus, equalTo(MedicationRequest.MedicationRequestStatus.STOPPED));
+	public void toFhirResource_shouldReturnStopped_whenDateStoppedAfterStart() throws Exception {
+		setDateStopped(drugOrder, today);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.STOPPED));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnStopped_whenDateStoppedSameMinuteAsStart() throws Exception {
+		setDateStopped(drugOrder, yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.STOPPED));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnStopped_whenDateStoppedInFutureAfterStart() throws Exception {
+		setDateStopped(drugOrder, tomorrow);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.STOPPED));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnStopped_whenBothStoppedAndAutoExpired() throws Exception {
+		setDateStopped(drugOrder, yesterday);
+		drugOrder.setAutoExpireDate(yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.STOPPED));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnCancelled_whenDateStoppedBeforeEffectiveStartDate() throws Exception {
+		drugOrder.setDateActivated(tomorrow);
+		setDateStopped(drugOrder, yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.CANCELLED));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnCancelled_whenDateStoppedBeforeScheduledStartDate() throws Exception {
+		drugOrder.setUrgency(Order.Urgency.ON_SCHEDULED_DATE);
+		drugOrder.setScheduledDate(tomorrow);
+		setDateStopped(drugOrder, yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.CANCELLED));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void toFhirResource_shouldThrowException_whenNoEffectiveStartDate() {
+		drugOrder.setDateActivated(null);
+		statusTranslator.toFhirResource(drugOrder);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void toFhirResource_shouldThrowException_whenMinimalOrderWithNoDates() {
+		DrugOrder minimalOrder = new DrugOrder();
+		minimalOrder.setUuid("minimal-uuid");
+		statusTranslator.toFhirResource(minimalOrder);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void toFhirResource_shouldThrowException_whenScheduledUrgencyButNoScheduledDate() {
+		drugOrder.setUrgency(Order.Urgency.ON_SCHEDULED_DATE);
+		drugOrder.setScheduledDate(null);
+		drugOrder.setDateActivated(null);
+		statusTranslator.toFhirResource(drugOrder);
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnCompleted_whenAutoExpireDateIsInPast() {
+		drugOrder.setAutoExpireDate(yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.COMPLETED));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenAutoExpireDateIsCurrentMinute() {
+		drugOrder.setAutoExpireDate(today);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenOrderIsCurrentlyActive() {
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenActivatedTodayWithNoEndDate() {
+		drugOrder.setDateActivated(today);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenAutoExpireDateIsInFuture() {
+		drugOrder.setAutoExpireDate(tomorrow);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenDateActivatedIsInFuture() {
+		drugOrder.setDateActivated(tomorrow);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenScheduledForFutureWithOnScheduledDateUrgency() {
+		drugOrder.setUrgency(Order.Urgency.ON_SCHEDULED_DATE);
+		drugOrder.setScheduledDate(tomorrow);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenScheduledDateIsYesterday() {
+		drugOrder.setUrgency(Order.Urgency.ON_SCHEDULED_DATE);
+		drugOrder.setScheduledDate(yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenActionIsRevise() {
+		drugOrder.setAction(Order.Action.REVISE);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenActionIsRenew() {
+		drugOrder.setAction(Order.Action.RENEW);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenUrgencyIsStatWithActiveOrder() {
+		drugOrder.setUrgency(Order.Urgency.STAT);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenUrgencyIsRoutineWithActiveOrder() {
+		drugOrder.setUrgency(Order.Urgency.ROUTINE);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnCompleted_whenFulfillerCompletedEvenIfNotExpired() {
+		drugOrder.setFulfillerStatus(Order.FulfillerStatus.COMPLETED);
+		drugOrder.setAutoExpireDate(tomorrow);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.COMPLETED));
+	}
+	
+	@Test
+	public void toFhirResource_shouldReturnActive_whenFulfillerInProgressEvenIfAutoExpired() {
+		drugOrder.setFulfillerStatus(Order.FulfillerStatus.IN_PROGRESS);
+		drugOrder.setAutoExpireDate(yesterday);
+		assertThat(statusTranslator.toFhirResource(drugOrder), equalTo(MedicationRequest.MedicationRequestStatus.ACTIVE));
+	}
+	
+	private void setDateStopped(DrugOrder drugOrder, Date dateStopped) throws Exception {
+		Field dateStoppedField = Order.class.getDeclaredField("dateStopped");
+		dateStoppedField.setAccessible(true);
+		dateStoppedField.set(drugOrder, dateStopped);
 	}
 }
