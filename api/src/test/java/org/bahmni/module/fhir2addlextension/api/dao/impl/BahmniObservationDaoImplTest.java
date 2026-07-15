5dbeebb353d81da6b03bc290b3fee46d082deeb0
skjameela276 <jameela.shaik@thoughtworks.com>
2026-07-15 16:20:32 +0530

    BAH-4858 | Update. improved coverage of BahmniObservationDaoImplTest,BahmniObservationSearchParamsTest

--- api/src/test/java/org/bahmni/module/fhir2addlextension/api/dao/impl/BahmniObservationDaoImplTest.java
+++ api/src/test/java/org/bahmni/module/fhir2addlextension/api/dao/impl/BahmniObservationDaoImplTest.java
@@ -1,8 +1,12 @@
 package org.bahmni.module.fhir2addlextension.api.dao.impl;
 
+import ca.uhn.fhir.rest.param.DateRangeParam;
 import ca.uhn.fhir.rest.param.ReferenceAndListParam;
 import ca.uhn.fhir.rest.param.ReferenceOrListParam;
 import ca.uhn.fhir.rest.param.ReferenceParam;
+import ca.uhn.fhir.rest.param.TokenAndListParam;
+import ca.uhn.fhir.rest.param.TokenOrListParam;
+import ca.uhn.fhir.rest.param.TokenParam;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +15,7 @@ import org.openmrs.Obs;
 import org.openmrs.module.fhir2.FhirConstants;
 import org.openmrs.module.fhir2.api.search.param.SearchParameterMap;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -209,6 +214,215 @@ public class BahmniObservationDaoImplTest {
 		SearchParameterMap theParams = new SearchParameterMap();
 		theParams.addParameter(FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER, basedOnReference);
 		theParams.addParameter(FhirConstants.COMMON_SEARCH_HANDLER, null);
+		
+		Obs observation = new Obs();
+		observation.setUuid(OBSERVATION_UUID);
+		
+		doReturn(Collections.singletonList(observation)).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, hasSize(1));
+	}
+	
+	@Test
+	public void shouldHandleBasedOnReferenceWithMultipleSlashes() {
+		ReferenceAndListParam basedOnReference = new ReferenceAndListParam().addAnd(new ReferenceOrListParam()
+		        .add(new ReferenceParam().setValue("http://fhir.server/ServiceRequest/" + ORDER_UUID)));
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER, basedOnReference);
+		
+		Obs observation = new Obs();
+		observation.setUuid(OBSERVATION_UUID);
+		
+		doReturn(Collections.singletonList(observation)).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, hasSize(1));
+	}
+	
+	@Test
+	public void shouldHandleBasedOnReferenceWithoutSlash() {
+		ReferenceAndListParam basedOnReference = new ReferenceAndListParam().addAnd(new ReferenceOrListParam()
+		        .add(new ReferenceParam().setValue(ORDER_UUID)));
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER, basedOnReference);
+		
+		Obs observation = new Obs();
+		observation.setUuid(OBSERVATION_UUID);
+		
+		doReturn(Collections.singletonList(observation)).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, hasSize(1));
+	}
+	
+	@Test
+	public void shouldHandleMultipleBasedOnReferencesInOrList() {
+		ReferenceAndListParam basedOnReference = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam().setValue("ServiceRequest/order-uuid-1"));
+		orListParam.add(new ReferenceParam().setValue("ServiceRequest/order-uuid-2"));
+		basedOnReference.addAnd(orListParam);
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER, basedOnReference);
+		
+		Obs observation1 = new Obs();
+		observation1.setUuid("obs-uuid-1");
+		Obs observation2 = new Obs();
+		observation2.setUuid("obs-uuid-2");
+		
+		doReturn(Arrays.asList(observation1, observation2)).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, hasSize(2));
+	}
+	
+	@Test
+	public void shouldHandleMultipleAndConditionsForBasedOn() {
+		ReferenceAndListParam basedOnReference = new ReferenceAndListParam();
+		basedOnReference.addAnd(new ReferenceOrListParam().add(new ReferenceParam().setValue(ORDER_UUID)));
+		basedOnReference.addAnd(new ReferenceOrListParam().add(new ReferenceParam()
+		        .setValue("ServiceRequest/another-order-uuid")));
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER, basedOnReference);
+		
+		doReturn(Collections.emptyList()).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+	}
+	
+	@Test
+	public void shouldHandleMultiplePatientReferencesInOrList() {
+		ReferenceAndListParam patientReference = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam().setValue("patient-uuid-1"));
+		orListParam.add(new ReferenceParam().setValue("patient-uuid-2"));
+		patientReference.addAnd(orListParam);
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.PATIENT_REFERENCE_SEARCH_HANDLER, patientReference);
+		
+		Obs observation1 = new Obs();
+		observation1.setUuid("obs-uuid-1");
+		Obs observation2 = new Obs();
+		observation2.setUuid("obs-uuid-2");
+		
+		doReturn(Arrays.asList(observation1, observation2)).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, hasSize(2));
+	}
+	
+	@Test
+	public void shouldHandleSearchWithLastUpdatedParameter() {
+		ReferenceAndListParam patientReference = new ReferenceAndListParam().addAnd(new ReferenceOrListParam()
+		        .add(new ReferenceParam().setValue(PATIENT_UUID)));
+		
+		DateRangeParam lastUpdated = new DateRangeParam();
+		lastUpdated.setLowerBound("2026-01-01");
+		lastUpdated.setUpperBound("2026-12-31");
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.PATIENT_REFERENCE_SEARCH_HANDLER, patientReference);
+		theParams.addParameter(FhirConstants.LAST_UPDATED_PROPERTY, lastUpdated);
+		
+		Obs observation = new Obs();
+		observation.setUuid(OBSERVATION_UUID);
+		
+		doReturn(Collections.singletonList(observation)).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, hasSize(1));
+	}
+	
+	@Test
+	public void shouldHandleSearchWithIdParameter() {
+		TokenAndListParam id = new TokenAndListParam();
+		id.addAnd(new TokenOrListParam().add(new TokenParam(OBSERVATION_UUID)));
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.COMMON_SEARCH_HANDLER, id);
+		
+		Obs observation = new Obs();
+		observation.setUuid(OBSERVATION_UUID);
+		
+		doReturn(Collections.singletonList(observation)).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, hasSize(1));
+	}
+	
+	@Test
+	public void shouldHandleEmptyBasedOnReferenceValue() {
+		ReferenceAndListParam basedOnReference = new ReferenceAndListParam().addAnd(new ReferenceOrListParam()
+		        .add(new ReferenceParam().setValue("")));
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER, basedOnReference);
+		
+		doReturn(Collections.emptyList()).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, empty());
+	}
+	
+	@Test
+	public void shouldHandleMixedReferenceFormats() {
+		ReferenceAndListParam basedOnReference = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam().setValue("ServiceRequest/order-with-prefix"));
+		orListParam.add(new ReferenceParam().setValue("order-without-prefix"));
+		basedOnReference.addAnd(orListParam);
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER, basedOnReference);
+		
+		Obs observation = new Obs();
+		observation.setUuid(OBSERVATION_UUID);
+		
+		doReturn(Collections.singletonList(observation)).when(spyDao).getSearchResults(theParams);
+		
+		List<Obs> results = spyDao.getSearchResults(theParams);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, hasSize(1));
+	}
+	
+	@Test
+	public void shouldHandleComplexMultiParameterSearch() {
+		ReferenceAndListParam patientReference = new ReferenceAndListParam().addAnd(new ReferenceOrListParam()
+		        .add(new ReferenceParam().setValue(PATIENT_UUID)));
+		ReferenceAndListParam basedOnReference = new ReferenceAndListParam().addAnd(new ReferenceOrListParam()
+		        .add(new ReferenceParam().setValue("ServiceRequest/" + ORDER_UUID)));
+		DateRangeParam lastUpdated = new DateRangeParam();
+		lastUpdated.setLowerBound("2026-01-01");
+		
+		SearchParameterMap theParams = new SearchParameterMap();
+		theParams.addParameter(FhirConstants.PATIENT_REFERENCE_SEARCH_HANDLER, patientReference);
+		theParams.addParameter(FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER, basedOnReference);
+		theParams.addParameter(FhirConstants.LAST_UPDATED_PROPERTY, lastUpdated);
 		
 		Obs observation = new Obs();
 		observation.setUuid(OBSERVATION_UUID);
--- api/src/test/java/org/bahmni/module/fhir2addlextension/api/search/param/BahmniObservationSearchParamsTest.java
+++ api/src/test/java/org/bahmni/module/fhir2addlextension/api/search/param/BahmniObservationSearchParamsTest.java
@@ -239,4 +239,247 @@ public class BahmniObservationSearchParamsTest {
 		SearchParameterMap paramMap = searchParams.toSearchParameterMap();
 		assertNotNull("Search parameter map should not be null even with no parameters", paramMap);
 	}
+	
+	@Test
+	public void shouldReturnFalseWhenPatientReferenceHasEmptyValue() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		ReferenceParam refParam = new ReferenceParam();
+		refParam.setValue("");
+		orListParam.add(refParam);
+		patientRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, null, null);
+		assertFalse("Should return false when patient reference has empty value", searchParams.hasPatientReference());
+	}
+	
+	@Test
+	public void shouldReturnFalseWhenBasedOnReferenceHasEmptyValue() {
+		ReferenceAndListParam basedOnRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		ReferenceParam refParam = new ReferenceParam();
+		refParam.setValue("");
+		orListParam.add(refParam);
+		basedOnRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(null, basedOnRef, null, null);
+		assertFalse("Should return false when basedOn reference has empty value", searchParams.hasBasedOnReference());
+	}
+	
+	@Test
+	public void shouldReturnFalseWhenPatientReferenceHasOnlyNullValues() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		ReferenceParam refParam = new ReferenceParam();
+		refParam.setValue(null);
+		orListParam.add(refParam);
+		patientRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, null, null);
+		assertFalse("Should return false when patient reference has only null values", searchParams.hasPatientReference());
+	}
+	
+	@Test
+	public void shouldReturnFalseWhenBasedOnReferenceHasOnlyNullValues() {
+		ReferenceAndListParam basedOnRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		ReferenceParam refParam = new ReferenceParam();
+		refParam.setValue(null);
+		orListParam.add(refParam);
+		basedOnRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(null, basedOnRef, null, null);
+		assertFalse("Should return false when basedOn reference has only null values", searchParams.hasBasedOnReference());
+	}
+	
+	@Test
+	public void shouldReturnFalseWhenMixedEmptyAndValidPatientReferencesInSameOrList() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam().setValue(""));
+		orListParam.add(new ReferenceParam("Patient", PATIENT_UUID));
+		patientRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, null, null);
+		assertFalse("Should return false when OR list contains both empty and valid values",
+		    searchParams.hasPatientReference());
+	}
+	
+	@Test
+	public void shouldReturnFalseWhenMixedEmptyAndValidBasedOnReferencesInSameOrList() {
+		ReferenceAndListParam basedOnRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam().setValue(""));
+		orListParam.add(new ReferenceParam("ServiceRequest", SERVICE_REQUEST_UUID));
+		basedOnRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(null, basedOnRef, null, null);
+		assertFalse("Should return false when OR list contains both empty and valid values",
+		    searchParams.hasBasedOnReference());
+	}
+	
+	@Test
+	public void shouldHandleMultipleAndListsWithMixedValues() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		// First AND condition with empty value
+		ReferenceOrListParam orList1 = new ReferenceOrListParam();
+		orList1.add(new ReferenceParam().setValue(""));
+		patientRef.addAnd(orList1);
+		// Second AND condition with valid value
+		ReferenceOrListParam orList2 = new ReferenceOrListParam();
+		orList2.add(new ReferenceParam("Patient", PATIENT_UUID));
+		patientRef.addAnd(orList2);
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, null, null);
+		assertTrue("Should return true with multiple AND lists containing valid values", searchParams.hasPatientReference());
+	}
+	
+	@Test
+	public void shouldReturnTrueForPatientReferenceWithResourceType() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam("Patient", PATIENT_UUID));
+		patientRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, null, null);
+		assertTrue("Should return true for patient reference with resource type", searchParams.hasPatientReference());
+	}
+	
+	@Test
+	public void shouldReturnTrueForBasedOnReferenceWithResourceType() {
+		ReferenceAndListParam basedOnRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam("ServiceRequest", SERVICE_REQUEST_UUID));
+		basedOnRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(null, basedOnRef, null, null);
+		assertTrue("Should return true for basedOn reference with resource type", searchParams.hasBasedOnReference());
+	}
+	
+	@Test
+	public void shouldVerifySearchParameterMapContainsPatientReference() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam("Patient", PATIENT_UUID));
+		patientRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, null, null);
+		SearchParameterMap paramMap = searchParams.toSearchParameterMap();
+		
+		assertNotNull("Search parameter map should not be null", paramMap);
+		assertTrue("Parameter map should contain patient reference",
+		    paramMap.getParameters().stream().anyMatch(p -> p.getKey().equals(org.openmrs.module.fhir2.FhirConstants.PATIENT_REFERENCE_SEARCH_HANDLER)));
+	}
+	
+	@Test
+	public void shouldVerifySearchParameterMapContainsBasedOnReference() {
+		ReferenceAndListParam basedOnRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam("ServiceRequest", SERVICE_REQUEST_UUID));
+		basedOnRef.addAnd(orListParam);
+		
+		searchParams = new BahmniObservationSearchParams(null, basedOnRef, null, null);
+		SearchParameterMap paramMap = searchParams.toSearchParameterMap();
+		
+		assertNotNull("Search parameter map should not be null", paramMap);
+		assertTrue("Parameter map should contain basedOn reference",
+		    paramMap.getParameters().stream().anyMatch(p -> p.getKey().equals(org.openmrs.module.fhir2.FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER)));
+	}
+	
+	@Test
+	public void shouldNotIncludeNullPatientReferenceInSearchParameterMap() {
+		searchParams = new BahmniObservationSearchParams(null, null, null, null);
+		SearchParameterMap paramMap = searchParams.toSearchParameterMap();
+		
+		assertNotNull("Search parameter map should not be null", paramMap);
+		assertFalse("Parameter map should not contain patient reference when null",
+		    paramMap.getParameters().stream().anyMatch(p -> p.getKey().equals(org.openmrs.module.fhir2.FhirConstants.PATIENT_REFERENCE_SEARCH_HANDLER)));
+	}
+	
+	@Test
+	public void shouldNotIncludeNullBasedOnReferenceInSearchParameterMap() {
+		searchParams = new BahmniObservationSearchParams(null, null, null, null);
+		SearchParameterMap paramMap = searchParams.toSearchParameterMap();
+		
+		assertNotNull("Search parameter map should not be null", paramMap);
+		assertFalse("Parameter map should not contain basedOn reference when null",
+		    paramMap.getParameters().stream().anyMatch(p -> p.getKey().equals(org.openmrs.module.fhir2.FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER)));
+	}
+	
+	@Test
+	public void shouldHandleChainedSortSpec() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam("Patient", PATIENT_UUID));
+		patientRef.addAnd(orListParam);
+		
+		SortSpec sort = new SortSpec("_lastUpdated");
+		SortSpec chainedSort = new SortSpec("date");
+		sort.setChain(chainedSort);
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, null, sort);
+		SearchParameterMap paramMap = searchParams.toSearchParameterMap();
+		assertNotNull("Search parameter map should handle chained sort", paramMap);
+	}
+	
+	@Test
+	public void shouldHandleEmptyDateRangeParam() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		ReferenceOrListParam orListParam = new ReferenceOrListParam();
+		orListParam.add(new ReferenceParam("Patient", PATIENT_UUID));
+		patientRef.addAnd(orListParam);
+		
+		DateRangeParam lastUpdated = new DateRangeParam();
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, lastUpdated, null);
+		SearchParameterMap paramMap = searchParams.toSearchParameterMap();
+		assertNotNull("Search parameter map should handle empty date range", paramMap);
+	}
+	
+	@Test
+	public void shouldVerifyGettersAndSetters() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		ReferenceAndListParam basedOnRef = new ReferenceAndListParam();
+		DateRangeParam lastUpdated = new DateRangeParam("2026-01-01", "2026-12-31");
+		SortSpec sort = new SortSpec("_lastUpdated");
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, basedOnRef, lastUpdated, sort);
+		
+		assertNotNull("Patient reference should not be null", searchParams.getPatientReference());
+		assertNotNull("BasedOn reference should not be null", searchParams.getBasedOnReference());
+		assertNotNull("Last updated should not be null", searchParams.getLastUpdated());
+		assertNotNull("Sort should not be null", searchParams.getSort());
+	}
+	
+	@Test
+	public void shouldHandleComplexPatientReferenceWithMultipleAndOrs() {
+		ReferenceAndListParam patientRef = new ReferenceAndListParam();
+		// First AND with multiple ORs
+		ReferenceOrListParam orList1 = new ReferenceOrListParam();
+		orList1.add(new ReferenceParam("Patient", "patient-uuid-1"));
+		orList1.add(new ReferenceParam("Patient", "patient-uuid-2"));
+		patientRef.addAnd(orList1);
+		// Second AND
+		ReferenceOrListParam orList2 = new ReferenceOrListParam();
+		orList2.add(new ReferenceParam("Patient", "patient-uuid-3"));
+		patientRef.addAnd(orList2);
+		
+		searchParams = new BahmniObservationSearchParams(patientRef, null, null, null);
+		assertTrue("Should handle complex patient reference with multiple AND/ORs", searchParams.hasPatientReference());
+	}
+	
+	@Test
+	public void shouldHandleComplexBasedOnReferenceWithMultipleAndOrs() {
+		ReferenceAndListParam basedOnRef = new ReferenceAndListParam();
+		ReferenceOrListParam orList1 = new ReferenceOrListParam();
+		orList1.add(new ReferenceParam("ServiceRequest", "order-uuid-1"));
+		orList1.add(new ReferenceParam("ServiceRequest", "order-uuid-2"));
+		basedOnRef.addAnd(orList1);
+		ReferenceOrListParam orList2 = new ReferenceOrListParam();
+		orList2.add(new ReferenceParam("ServiceRequest", "order-uuid-3"));
+		basedOnRef.addAnd(orList2);
+		
+		searchParams = new BahmniObservationSearchParams(null, basedOnRef, null, null);
+		assertTrue("Should handle complex basedOn reference with multiple AND/ORs", searchParams.hasBasedOnReference());
+	}
 }
