d2e542f8a61b98899494fc8074511c159c8a559c
MOHANKUMAR T <mohankumar.t@thoughtworks.com>
2026-08-19 15:00:29 +0530

    BAH-4997 | Fix. Incorrect inheritance to Obs DAO implementation

--- api/src/main/java/org/bahmni/module/fhir2addlextension/api/dao/impl/BahmniObsDaoImpl.java
+++ api/src/main/java/org/bahmni/module/fhir2addlextension/api/dao/impl/BahmniObsDaoImpl.java
@@ -6,8 +6,9 @@ import org.hibernate.Criteria;
 import org.hibernate.query.NativeQuery;
 import org.openmrs.Obs;
 import org.openmrs.module.fhir2.FhirConstants;
-import org.openmrs.module.fhir2.api.dao.impl.BaseFhirDao;
+import org.openmrs.module.fhir2.api.dao.impl.FhirObservationDaoImpl;
 import org.openmrs.module.fhir2.api.search.param.SearchParameterMap;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,23 +16,17 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
-public class BahmniObsDaoImpl extends BaseFhirDao<Obs> implements BahmniObsDao {
+@Primary
+public class BahmniObsDaoImpl extends FhirObservationDaoImpl implements BahmniObsDao {
 	
 	@Override
 	protected void setupSearchParams(Criteria criteria, SearchParameterMap theParams) {
 		super.setupSearchParams(criteria, theParams);
 		theParams.getParameters().forEach(param -> {
 			switch (param.getKey()) {
-				case FhirConstants.PATIENT_REFERENCE_SEARCH_HANDLER:
-					param.getValue().forEach(patientReference -> handlePatientReference(criteria,
-					    (ReferenceAndListParam) patientReference.getParam(), "person"));
-					break;
 				case FhirConstants.BASED_ON_REFERENCE_SEARCH_HANDLER:
 					param.getValue().forEach(basedOnReference -> handleBasedOnReference(criteria,
 					    basedOnReference.getParam()));
-					break;
-				case FhirConstants.COMMON_SEARCH_HANDLER:
-					handleCommonSearchParameters(param.getValue()).ifPresent(criteria::add);
 					break;
 			}
 		});
