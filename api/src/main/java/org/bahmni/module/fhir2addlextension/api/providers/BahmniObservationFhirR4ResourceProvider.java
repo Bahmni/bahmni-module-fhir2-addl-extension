f5ec61cc2f86732c23d7c0f7380104ea23acfcec
MOHANKUMAR T <mohankumar.t@thoughtworks.com>
2026-08-25 07:24:59 +0530

    BAH-5016 | Add. Encounter filtering for last-n observations

--- api/src/main/java/org/bahmni/module/fhir2addlextension/api/providers/BahmniObservationFhirR4ResourceProvider.java
+++ api/src/main/java/org/bahmni/module/fhir2addlextension/api/providers/BahmniObservationFhirR4ResourceProvider.java
@@ -1,6 +1,9 @@
 package org.bahmni.module.fhir2addlextension.api.providers;
 
+import ca.uhn.fhir.model.api.Include;
 import ca.uhn.fhir.model.api.annotation.Description;
+import ca.uhn.fhir.model.valueset.BundleTypeEnum;
+import ca.uhn.fhir.rest.annotation.IncludeParam;
 import ca.uhn.fhir.rest.annotation.Operation;
 import ca.uhn.fhir.rest.annotation.OperationParam;
 import ca.uhn.fhir.rest.annotation.OptionalParam;
@@ -10,8 +13,11 @@ import ca.uhn.fhir.rest.api.SortSpec;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.param.DateRangeParam;
+import ca.uhn.fhir.rest.param.NumberParam;
 import ca.uhn.fhir.rest.param.ReferenceAndListParam;
+import ca.uhn.fhir.rest.param.TokenAndListParam;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import org.apache.commons.collections.CollectionUtils;
 import org.bahmni.module.fhir2addlextension.api.context.RequestContextHolder;
 import org.bahmni.module.fhir2addlextension.api.search.param.BahmniObservationSearchParams;
 import org.bahmni.module.fhir2addlextension.api.service.BahmniFhirObservationService;
@@ -19,9 +25,12 @@ import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
 import org.openmrs.module.fhir2.api.annotations.R4Provider;
+import org.openmrs.module.fhir2.api.search.param.ObservationSearchParams;
 import org.openmrs.module.fhir2.providers.r4.ObservationFhirResourceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
 
 @Component("bahmniObservationFhirR4ResourceProvider")
 @R4Provider
@@ -56,5 +65,45 @@ public class BahmniObservationFhirR4ResourceProvider extends ObservationFhirReso
 		searchParams.setEncounterReference(encounterReference);
 		searchParams.setBasedOnReference(basedOnReference);
 		return observationService.fetchAllObservation(searchParams);
+	}
+	
+	/*
+	- This override has been done to introduce additional Encounter param and include param to support filtering of Obs
+	for a visit/episode.
+	- HAPI Server does not support overrides of Operation Methods and it leads to the parent operation always being called. So this
+	override changes the operation name from lastn to last-n.
+	TODO: Remove this override once https://openmrs.atlassian.net/browse/FM2-697 is resolved and merged and module version is upgraded.
+
+	 */
+	@Operation(name = "last-n", idempotent = true, type = Observation.class, bundleType = BundleTypeEnum.SEARCHSET)
+	public IBundleProvider getLastnObservations(
+	        @OperationParam(name = "max") NumberParam max,
+	        @OperationParam(name = Observation.SP_SUBJECT) ReferenceAndListParam subjectParam,
+	        @OperationParam(name = Observation.SP_PATIENT) ReferenceAndListParam patientParam,
+	        @OperationParam(name = Observation.SP_CATEGORY) TokenAndListParam category,
+	        @OperationParam(name = Observation.SP_CODE) TokenAndListParam code,
+	        @OperationParam(name = Observation.SP_ENCOUNTER) ReferenceAndListParam encounterParam,
+	        @IncludeParam(allow = { "Observation:" + Observation.SP_ENCOUNTER, "Observation:" + Observation.SP_PATIENT,
+	                "Observation:" + Observation.SP_HAS_MEMBER }) HashSet<Include> includes,
+	        @IncludeParam(reverse = true, allow = { "Observation:" + Observation.SP_HAS_MEMBER }) HashSet<Include> revIncludes) {
+		if (patientParam != null) {
+			subjectParam = patientParam;
+		}
+		if (CollectionUtils.isEmpty(includes)) {
+			includes = null;
+		}
+		if (CollectionUtils.isEmpty(revIncludes)) {
+			revIncludes = null;
+		}
+		
+		ObservationSearchParams searchParams = new ObservationSearchParams();
+		searchParams.setPatient(subjectParam);
+		searchParams.setCategory(category);
+		searchParams.setEncounter(encounterParam);
+		searchParams.setCode(code);
+		searchParams.setIncludes(includes);
+		searchParams.setRevIncludes(revIncludes);
+		
+		return observationService.getLastnObservations(max, searchParams);
 	}
 }
