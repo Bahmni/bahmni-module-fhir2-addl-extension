35360740c90c163d3b9d6fd26aa8404c8e41cbed
ravinderkabli <ravinder.kabli@thoughtworks.com>
2026-09-07 17:04:14 +0530

    Revert "BAH-5021 | Fix Javadoc build failure caused by Lombok onMethod_ annot…" (#123)
    This reverts commit 77bb11cb507ea57fa312133cddac092b17500158.

--- api/src/main/java/org/bahmni/module/fhir2addlextension/api/search/BahmniSearchQueryIncludeImpl.java
+++ api/src/main/java/org/bahmni/module/fhir2addlextension/api/search/BahmniSearchQueryIncludeImpl.java
@@ -4,6 +4,7 @@ import ca.uhn.fhir.model.api.Include;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.param.ReferenceAndListParam;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bahmni.module.fhir2addlextension.api.BahmniFhirConstants;
@@ -28,10 +29,10 @@ public class BahmniSearchQueryIncludeImpl<U extends IBaseResource> extends Searc
 	
 	private static final Log log = LogFactory.getLog(BahmniSearchQueryIncludeImpl.class);
 	
-	@Autowired
+	@Setter(onMethod_ = @Autowired)
 	private BahmniFhirEpisodeOfCareEncounterService episodeOfCareEncounterService;
 	
-	@Autowired
+	@Setter(onMethod_ = @Autowired)
 	private BahmniFhirImagingStudyService imagingStudyService;
 	
 	@Override
